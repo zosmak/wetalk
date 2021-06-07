0388a46502da3cf931d8e75203c46e43bf7a4517
Daniel Eleutério <daniel.eleuterio@outlook.pt>
2021-06-07 02:33:26 +0100

    chore: add files migration

--- wetalkAPI/wetalkAPI/Entities/UserFile.cs
+++ wetalkAPI/wetalkAPI/Entities/UserFile.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WetalkAPI.Entities
 {
     public class UserFile
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public string FileName { get; set; }
+
         public int UserID { get; set; }
         public User User { get; set; }
     }
---
+++ wetalkAPI/wetalkAPI/Migrations/20210607013147_Files.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WetalkAPI.Migrations
+{
+    public partial class Files : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserFiles",
+                columns: table => new
+                {
+                    FileName = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    UserID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserFiles", x => x.FileName);
+                    table.ForeignKey(
+                        name: "FK_UserFiles_Users_UserID",
+                        column: x => x.UserID,
+                        principalTable: "Users",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserFiles_UserID",
+                table: "UserFiles",
+                column: "UserID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserFiles");
+        }
+    }
+}
